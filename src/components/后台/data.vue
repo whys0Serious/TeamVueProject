17b48f0976417c2405ce12c4c64897a195b9c4a1
zqzhuzhuxia <13772692011@163.com>
2019-10-11 01:04:03 +0800

    展示 echart

---
+++ src/components/后台/data.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <div id="addcouse">
+
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style>
+
+  #addcouse{
+    width:1360px;
+    height: 600px;
+    float: left;
+  }
+</style>
